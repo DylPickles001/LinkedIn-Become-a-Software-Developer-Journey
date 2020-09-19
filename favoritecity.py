9a0ea6d0dc4688769a7290f33e842a30413595e1
DylPickles001 <56860046+DylPickles001@users.noreply.github.com>
2020-09-19 16:22:25 -0700

    added favoritecity.py
    Included Section 5 (Modular code) challenge code.

---
+++ favoritecity.py
@@ -0,0 +1,12 @@
+#Section 5 (Modular Code) Favorite Cities Challenge
+#program prints out the name of favorite cities
+#(MUST HAVE 1 function): has a function named favorite_city
+# favorite_city should have 1 parameter: name
+# favorite_city should be called at least 3 times
+#output- "One of my favorite cities is: "
+
+def favorite_city (name):
+    print("One of my favorite cities is", name)
+favorite_city("Los Angeles, California")
+favorite_city("Whittier, California")
+favorite_city("Irvine, California")
